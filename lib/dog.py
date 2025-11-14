df2714d974eda7fb2fbe37c6a8b91c41fa9b4c51
bran259 <kiprop686@gmail.com>
2025-11-14 23:33:46 +0300

    attributes ad properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    APPROVED_BREEDS = ["Pug", "German Sherpherd", "Bulldog", "Golden Retriever"]
+
+    def __init__(self, name=None, breed=None):
+        if name is not None:
+            if type(name)is not str or len(name) < 1 or len(name) > 25:
+                print("Name must be string between 1 and 25 characters.")
+        self.name = name  
+
+        if breed is not None:
+            if breed not in self.APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")  
+        self.breed = breed        
     pass
